aed2d6e5fa471dea9d585128eb7d1b052fcbd484
Alex Bykov <alex@bykov.me>
2020-03-28 00:52:04 +0300

    vulnerability fixed

--- impl_none_test.go
+++ impl_none_test.go
@@ -2,28 +2,27 @@ package bsencrypt
 
 import "testing"
 
-const cNONEInitialText = "en47DD767d4bdjbrjhJHBHDJ"
-const cNONEPassword = "drdnrd"
-
 func TestNoneEncrypt(t *testing.T) {
 	// As there is no encryption, check if it is the same
 	var none cypherNONE
-	err := none.SetPassword(cNONEPassword)
+	genInitialText := generateRandomString(40, 100)
+	genPass := generateRandomString(5, 32)
+	err := none.SetPassword(genPass)
 	if err != nil {
 		t.Error(err.Error())
 	}
-	encrypted, err := none.Encrypt(cNONEInitialText)
+	encrypted, err := none.Encrypt(genInitialText)
 	if err != nil {
 		t.Error(err.Error())
 	}
-	if encrypted != cNONEInitialText {
-		t.Errorf("Expected %s. retrieved %s", cNONEInitialText, encrypted)
+	if encrypted != genInitialText {
+		t.Errorf("Expected %s. retrieved %s", genInitialText, encrypted)
 	}
 	decrypted, err := none.Decrypt(encrypted)
 	if err != nil {
 		t.Error(err.Error())
 	}
 	if encrypted != decrypted {
-		t.Errorf("Expected %s. retrieved %s", cNONEInitialText, decrypted)
+		t.Errorf("Expected %s. retrieved %s", genInitialText, decrypted)
 	}
 }
